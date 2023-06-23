24726572f4b7f944a5539c18ca1cb15dce712c1a
pizzaroot <pizzaroot@sju.ac.kr>
2023-06-23 22:22:21 +0900

    first commit

---
+++ MFCPaint/resource.h
@@ -0,0 +1,25 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCPaint.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCPaintTYPE                130
+#define ID_ACTION_DRAW                  32771
+#define ID_ACTION_SELECT                32772
+#define ID_DRAW_RECTANGLE               32773
+#define ID_DRAW_CIRCLE                  32774
+#define ID_DRAW_CURVE                   32775
+#define ID_DRAW_STAR                    32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
---
+++ README.md
@@ -0,0 +1,11 @@
+# MFCPaint
+MFC식 그림판
+
+## Supported OS
+- Windows 10 or higher
+
+## Build requirements
+- Visual Studio 2022 or higher with MFC installed
+
+## License
+MFCPaint is licensed under the MIT
